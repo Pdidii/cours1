c9f7b51f31c52ac32c4e084a211480cf501ff116
Dimitri <dimitriweber92@gmail.com>
2019-06-18 16:49:10 +0200

    Update Style Défi

--- deuxiemepage/script2.js
+++ deuxiemepage/script2.js
@@ -4,3 +4,18 @@ $( document ).ready(function() {
    $("#welcome").html('<h4>Bien le bonjour, <br> soyez pret à casser la baraque !</h4>');
 };
 })
+
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}
+
+ function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+};
+// Appel la fonction
+welcome();
